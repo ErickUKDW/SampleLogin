ab47eb44682dbd2453b061aa47a2c7dfe3a548c2
erick <erick@DESKTOP-I963S2R>
2016-11-08 15:07:24 +0700

    contoh penggunaan hash

--- SampleLogin/DAL/PenggunaDAL.cs
+++ SampleLogin/DAL/PenggunaDAL.cs
@@ -5,10 +5,29 @@ using System.Web;
 
 using SampleLogin.Models;
 using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SampleLogin.DAL
 {
     public class PenggunaDAL
     {
+        private string GetConnStr()
+        {
+            return @"Data Source=.\SQLEXPRESS;
+                     Initial Catalog=SampleShopDb;
+                     Integrated Security=True";
+        }
+
+        public string GetMD5Hash(string password)
+        {
+            UnicodeEncoding unicode = new UnicodeEncoding();
+            var dataHash = unicode.GetBytes(password);
+            MD5 md5 = new MD5CryptoServiceProvider();
+            var hasil = md5.ComputeHash(dataHash);
+            return Convert.ToBase64String(hasil);
+        }
+
+
     }
 }
