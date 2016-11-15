7c17da0a2d8c5daafa38ef7e0f6fa2072977984b
erick <erick@DESKTOP-I963S2R>
2016-11-15 15:10:41 +0700

    fitur login

--- SampleLogin/Controllers/PenggunaController.cs
+++ SampleLogin/Controllers/PenggunaController.cs
@@ -14,12 +14,44 @@ namespace SampleLogin.Controllers
         // GET: Pengguna
         public ActionResult Index()
         {
-            return View();
+            //cek apakah user sudah login
+            //jika user belum login
+            if(Session["pengguna"]==null)
+            {
+                TempData["pesan"] = 
+                    "<div class='alert alert-info'>Anda harus melakukan Login terlebih dahulu</div>";
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                Pengguna objPengguna = (Pengguna)Session["pengguna"];
+                return View(objPengguna);
+            }
+            
         }
 
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Pengguna pengguna)
+        {
+            PenggunaDAL penggunaDAL = new PenggunaDAL();
+            var objPengguna = penggunaDAL.LoginPengguna(pengguna);
+
+            if(objPengguna!=null)
+            {
+                //jika login berhasil bautkan session baru
+                Session["pengguna"] = objPengguna;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Error = "<div class='alert alert-danger'>Login Gagal !</div>";
+                return View();
+            }
         }
     }
 }
