dafae5b44f78baa2558e0f16050ddd49445e82ca
erick <erick@DESKTOP-I963S2R>
2016-11-15 15:58:52 +0700

    menambahkan proteksi halaman Admin

---
+++ SampleLogin/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using SampleLogin.Models;
+using SampleLogin.DAL;
+
+namespace SampleLogin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            if (Session["pengguna"] == null)
+            {
+                return RedirectToAction("Login", "Pengguna");
+            }
+            else
+            {
+                var objPengguna = (Pengguna)Session["pengguna"];
+                if (objPengguna.Aturan != "Admin")
+                {
+                    TempData["pesan"] =
+                    @"<div class='alert alert-info'>Anda harus melakukan Login 
+                      sebagai admin terlebih dahulu</div>";
+                    return RedirectToAction("Login", "Pengguna");
+                }
+                else
+                {
+                    return View(objPengguna);
+                }
+            }
+
+        }
+    }
+}
