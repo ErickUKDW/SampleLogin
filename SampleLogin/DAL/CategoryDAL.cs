a040f9514b1fcfe2de3d9429483799906d55460b
erick <erick@DESKTOP-I963S2R>
2016-11-22 14:31:44 +0700

    GetAll Category with Dapper

---
+++ SampleLogin/DAL/CategoryDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using SampleLogin.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+using Dapper;
+
+namespace SampleLogin.DAL
+{
+    public class CategoryDAL
+    {
+        private string GetConnStr()
+        {
+            return ConfigurationManager
+                .ConnectionStrings["SampleConnectionString"]
+                .ConnectionString;
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"select * from Categories 
+                                  order by CategoryName";
+
+                return conn.Query<Category>(strSql);
+            }
+        }
+    }
+}
